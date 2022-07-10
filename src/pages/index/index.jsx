11ab0923cba0a71a2ade7b6ecd0c7efbea23e594
Satus Space <SatusSpaceProa@gmail.com>
2022-07-10 17:48:35 -0300

    Update

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -21,11 +21,40 @@ export const Index = () => {
                     </div>
                 </div>
                 <div className="containerInfo row">
-                    <div className="pacAgendados">
-                        <h1>1</h1>
+                    <div className="info color1">
+                        <h1 className="txtN">20</h1>
                         <h4>Pacientes agendados</h4>
                     </div>
+                    <div className="info color2">
+                        <h1 className="txtN">12</h1>
+                        <h4>Pacientes confirmados</h4>
+                    </div>
+                    <div className="info color3">
+                        <h1 className="txtN">10</h1>
+                        <h4>Pacientes atendidos</h4>
+                    </div>
+                    <div className="info color4">
+                        <h1 className="txtN">3</h1>
+                        <h4>Pacientes que faltaram</h4>
+                    </div>
                 </div>
+                
+                <div className="dashboard">
+                    <div className="infoGraphic">
+                        <h4>Pacientes</h4>
+                    </div>
+
+                    <div className="infoGraphic">
+                        <h4>Especialidades</h4>
+                    </div>
+
+                    <div className="infoGraphic">
+                        <h4>Conveniados</h4>
+                    </div>
+
+                    
+                </div>
+
                 
             </div>
             
